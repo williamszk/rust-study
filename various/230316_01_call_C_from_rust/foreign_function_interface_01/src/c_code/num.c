d20ab1e7a5b21e064dfc90b61bbb220e28032436
williamszk <wllmszk@gmail.com>
2023-03-17 08:31:51 -0300

    rust C ffi study

---
+++ various/230316_01_call_C_from_rust/foreign_function_interface_01/src/c_code/num.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void print_num(int num){
+    printf("[  c ] num is %d\n", num);
+}
---
+++ various/230316_03_experiments_rustnomicon_ffi/notes.sh
@@ -0,0 +1,9 @@
+
+# The code in here is partially related to the Rustnomicon
+
+cargo new project_01
+cd project_01
+cargo run
+
+
+
