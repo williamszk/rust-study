3770f588259946a0ff40033f8c419884e25c8538
williamszk <wllmszk@gmail.com>
2023-03-18 19:52:54 -0300

    rust study, the book

--- book-the-rust-programming-language/230311_01/notes.sh
+++ book-the-rust-programming-language/230311_01/notes.sh
@@ -1,5 +1,12 @@
 
-cargo new owernership
-cd owernership
+# Try making the code run without using the Copy trait in the generic notation.
+# Try using the Clone trait.
+# And try returning the reference from the function.
 
+cargo new traits_01
+cd traits_01
+cargo run
+
+cargo new traits_02
+cd traits_02
 cargo run
---
+++ book-the-rust-programming-language/230311_01/traits_02/src/main.rs
@@ -0,0 +1,20 @@
+// Solution without using Copy trait inside the generic type T
+
+fn largest<T: PartialOrd+Clone>(list: &[T]) -> T {
+    let mut largest = list[0].clone();
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+}
