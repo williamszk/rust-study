f6e3d3f7a3c38d9f4dcdcc2b15f42e3e52e560a3
williamszk <wllmszk@gmail.com>
2023-04-25 01:10:08 +0000

    rust study, change dir name eshwarla

--- youtube-coding-tech/230418_yew_stream/notes.sh
+++ youtube-coding-tech/230418_yew_stream/notes.sh
@@ -1,4 +1,5 @@
 # https://www.youtube.com/watch?v=lJllt5X6ELg&list=PLFjq8z-aGyQ6t_LGp7wqHsHTYO-pDDx84&index=32&ab_channel=CodingTech
+# https://youtu.be/lJllt5X6ELg?t=348
 
 # to serve the web app through the browser
 cargo install trunk
@@ -8,10 +9,9 @@ rustup target add wasm32-unknown-unknown
 cargo new yew-video-streaming
 cd yew-video-streaming
 
-cargo run
-
-
 touch index.html style.css
 
+# =============================================================================
 # to run the yew server
+cd yew-video-streaming
 trunk serve
