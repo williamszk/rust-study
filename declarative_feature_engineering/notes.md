24d8b7ee3024bf7cdb57dc4df42e0acabdc8d2ab
williamszk <wllmszk@gmail.com>
2023-07-11 15:45:54 +0000

    include new code for data pipeline, and parsing in Rust

--- declarative_feature_engineering/notes.md
+++ declarative_feature_engineering/notes.md
@@ -7,14 +7,14 @@ https://docs.google.com/document/d/1YZwnJHrZtYg3SDLCqveh_rKjXU7FofT_DxyGPdggfxk/
 
 ## To-do
 
-- [ ] Build a data pipeline in Pandas
-- [ ] Take a look at: https://analyticsindiamag.com/10-datasets-for-data-cleaning-practice-for-beginners/
-- [ ] https://www.google.com/search?q=where+to+find+datasets+to+practice+data+wrangling&oq=where+to+find+datasets+to+practice+data+wrangling&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIHCAEQIRigAdIBCTExNTE5ajBqNKgCALACAA&sourceid=chrome&ie=UTF-8
 - [ ] One of the most important functionalities is the ability to import csv.
 - [ ] Use this pipeline as an example to build the YAML features for the declarative language
 - [ ] Study how to create a programming language.
 - [ ] Study how to create a declarative programming language.
 - [ ] Study how to create a transpiler.
+- [x] Build a data pipeline in Pandas
+- [ ] Take a look at: https://analyticsindiamag.com/10-datasets-for-data-cleaning-practice-for-beginners/
+- [ ] https://www.google.com/search?q=where+to+find+datasets+to+practice+data+wrangling&oq=where+to+find+datasets+to+practice+data+wrangling&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIHCAEQIRigAdIBCTExNTE5ajBqNKgCALACAA&sourceid=chrome&ie=UTF-8
 - [x] Learn how to use Rust to process the same YAML file
 - [x] Work through this tutorial: https://www.cloudbees.com/blog/yaml-tutorial-everything-you-need-get-started
 - [ ] Create a YAML file in which we can define steps for a feature engineering
--- declarative_feature_engineering/proj/src/main.rs
+++ declarative_feature_engineering/proj/src/main.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 use serde::de::DeserializeOwned;
 use serde::Deserialize;
 use serde_yaml;
@@ -9,13 +11,35 @@ use std::io::Read;
 fn main() -> Result<(), Box<dyn error::Error>> {
     println!("Hello, world!");
     // ------------------------------------------------------------------------
-    print_yaml_struct_known::<MyData>("./src/foo2.yaml");
+    // print_yaml_struct_known::<MyData>("./src/foo2.yaml");
     // ------------------------------------------------------------------------
     // print_yaml_struct_unknown("./src/foo3.yaml");
     // print_yaml_struct_unknown("./src/experiments/230708_01/experiment_with_table.yaml");
-    print_yaml_struct_unknown("./src/experiments/230708_01/empty_file.yaml");
+    // print_yaml_struct_unknown("./src/experiments/230708_01/empty_file.yaml");
+    // print_yaml_struct_unknown("./src/experiments/230711_01/test01.yaml");
+    let yaml_content = read_yaml("./src/experiments/230711_01/test01.yaml");
+    // println!("{:#?}", yaml_content);
+
+    match yaml_content {
+        serde_yaml::Value::Mapping(mapping) => {
+            println!("{:?}", mapping.len());
+            println!("{:?}", mapping);
+        }
+        _ => println!("Hi"),
+    }
 
     Ok(())
+}
+
+fn read_yaml(file_path: &str) -> serde_yaml::Value {
+    // An example using serde_yaml NOT knowing the structure in advance
+    let file = File::open(file_path).expect("Failed to open the file.");
+
+    // Parse the YAML file into a serde_yaml::Value object
+    let yaml_content: serde_yaml::Value =
+        serde_yaml::from_reader(file).expect("Failed to parse YAML.");
+
+    return yaml_content;
 }
 
 fn print_yaml_struct_unknown(file_path: &str) {
