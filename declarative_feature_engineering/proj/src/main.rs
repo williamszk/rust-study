9e95fea15c136b1ae6042971e7d1f499cecf47b7
williamszk <wllmszk@gmail.com>
2023-07-11 23:27:14 +0000

    get values from Mapping in yaml, change serde_yaml version

--- declarative_feature_engineering/proj/src/main.rs
+++ declarative_feature_engineering/proj/src/main.rs
@@ -3,6 +3,8 @@
 use serde::de::DeserializeOwned;
 use serde::Deserialize;
 use serde_yaml;
+use serde_yaml::Value;
+use std::collections::HashMap;
 use std::error;
 use std::fmt::Debug;
 use std::fs::File;
@@ -17,27 +19,36 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     // print_yaml_struct_unknown("./src/experiments/230708_01/experiment_with_table.yaml");
     // print_yaml_struct_unknown("./src/experiments/230708_01/empty_file.yaml");
     // print_yaml_struct_unknown("./src/experiments/230711_01/test01.yaml");
-    let yaml_content = read_yaml("./src/experiments/230711_01/test01.yaml");
-    // println!("{:#?}", yaml_content);
 
-    match yaml_content {
-        serde_yaml::Value::Mapping(mapping) => {
-            println!("{:?}", mapping.len());
-            println!("{:?}", mapping);
+    let yaml_content = read_yaml("./src/experiments/230711_01/test01.yaml");
+
+    // I should create a map in rust to handle the ids that I already used
+    let mut all_ids: HashMap<String, Box<dyn Fn() -> String>> = HashMap::new();
+    // The value of the HashMap is a function which does not receive anything and returns a string.
+
+    if let Value::Mapping(mapping) = yaml_content {
+        for key in mapping.keys() {
+            println!("{:?}", key);
+            let value = mapping.get(key).expect("Couldn't find the key!");
+            println!("{:?}", value);
         }
-        _ => println!("Hi"),
     }
 
     Ok(())
 }
 
-fn read_yaml(file_path: &str) -> serde_yaml::Value {
+fn print_keys_from_mapping(mapping: serde_yaml::Mapping) {
+    for key in mapping.keys() {
+        println!("{:?}", key);
+    }
+}
+
+fn read_yaml(file_path: &str) -> Value {
     // An example using serde_yaml NOT knowing the structure in advance
     let file = File::open(file_path).expect("Failed to open the file.");
 
     // Parse the YAML file into a serde_yaml::Value object
-    let yaml_content: serde_yaml::Value =
-        serde_yaml::from_reader(file).expect("Failed to parse YAML.");
+    let yaml_content: Value = serde_yaml::from_reader(file).expect("Failed to parse YAML.");
 
     return yaml_content;
 }
