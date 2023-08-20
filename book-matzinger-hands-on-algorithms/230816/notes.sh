9117539d5b3ad077cd95df46039bab87ea72c827
williamszk <wllmszk@gmail.com>
2023-08-20 20:16:51 +0000

    rust study, algo data structure

--- book-matzinger-hands-on-algorithms/230816/notes.sh
+++ book-matzinger-hands-on-algorithms/230816/notes.sh
@@ -10,9 +10,17 @@ cd proj02
 cargo run
 cargo test
 
+cargo new proj03 --lib
+cd proj03
+cargo test
+cargo bench
 
 
-
+# how to change the version of rust compiler ----------------------------------
+# install nightly toolchain
+rustup toolchain install nightly
+# change the rust compiler to use the nightly version
+rustup default nightly
 
 
 
--- book-matzinger-hands-on-algorithms/230816/proj02/src/main.rs
+++ book-matzinger-hands-on-algorithms/230816/proj02/src/main.rs
@@ -5,7 +5,12 @@ fn main() {
     println!("{:?}", my_var);
     let my_var = another_function(my_var);
     println!("{:?}", my_var);
+    // my_add(std::i32::MAX, std::i32::MAX);
 }
+
+// pub fn my_add(left: i32, right: i32) -> i32 {
+//     left + right
+// }
 
 fn another_function(mut passing_through: MyStruct) -> MyStruct {
     let x = vec![11, 12, 13];
