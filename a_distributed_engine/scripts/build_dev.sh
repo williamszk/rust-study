591c5127c3eba753cace3ef77f02310880f3d716
williamszk <wllmszk@gmail.com>
2023-08-25 13:34:04 +0000

    remove copy and delete of executables when building image

--- a_distributed_engine/scripts/build_dev.sh
+++ a_distributed_engine/scripts/build_dev.sh
@@ -1,15 +1,10 @@
 #! /bin/bash
 
-# compile the executables
+# compile the executables ------------------------------------------------------
 cd dustr
 cargo build
 cd -
 
-# create images
-cp ./dustr/target/debug/worker ./worker
+# create images ----------------------------------------------------------------
 docker build -t dustr-worker-dev -f scripts/Dockerfile.worker.dev .
-rm worker
-
-cp ./dustr/target/debug/manager ./manager
 docker build -t dustr-manager-dev -f scripts/Dockerfile.manager.dev .
-rm manager
